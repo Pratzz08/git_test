04663ec98a4d350e74e7fb239ed4370206fb5333
pratzz <166483259+Pratzz08@users.noreply.github.com>
2025-11-05 20:12:37 +0530

    Add index.html and main.js file to practice js

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+let name = "jhon";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+const max = 57;
+const actual = max-13;
+const percentage = actual/max;
+console.log(percentage);
